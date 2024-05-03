698761a9b37f0599cb177bb60d095830ce386229
João Paulo Dagostin <dagostinjota@gmail.com>
2024-05-03 10:47:57 +0200

    update pages

---
+++ src/pages/Inicio/index.tsx
@@ -0,0 +1,10 @@
+import cardapio from "pages/Cardapio/Itens/itens.json";
+
+export default function Inicio() {
+  return (
+    <section>
+      <h3>Recomendacoes da cozinha</h3>
+      <div></div>
+    </section>
+  );
+}
