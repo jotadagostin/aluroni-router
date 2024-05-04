8612098322ed32053ac3395d5c6ba925e283108d
João Paulo Dagostin <dagostinjota@gmail.com>
2024-05-04 11:56:39 +0200

    Update index.tsx
    setting routes and index

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import "./index.css";
-import Inicio from "pages/Inicio";
-import Cardapio from "pages/Cardapio";
-import Router from "./routes";
 
-const componenteAtual =
-  window.location.pathname === "/" ? <Inicio /> : <Cardapio />;
+import Router from "./routes";
 
 ReactDOM.render(
   <React.StrictMode>
